7c9ee5f44c11f3c95d3c6b45e5d761b178317960
Dan Alekseev <i@qrd.wtf>
2024-04-03 11:36:43 +0400

    fix: export boosted_by_async

--- axum-htmx-derive-boosted-by/src/lib.rs
+++ axum-htmx-derive-boosted-by/src/lib.rs
@@ -1,11 +1,18 @@
 #![doc = include_str!("../README.md")]
-
-use axum_htmx_derive_core::boosted_by;
 use proc_macro::TokenStream;
 use proc_macro_error::proc_macro_error;
+
+use axum_htmx_derive_core::boosted_by;
+use axum_htmx_derive_core::boosted_by_async;
 
 #[proc_macro_error]
 #[proc_macro_attribute]
 pub fn hx_boosted_by(args: TokenStream, input: TokenStream) -> TokenStream {
     boosted_by(args.into(), input.into()).into()
+}
+
+#[proc_macro_error]
+#[proc_macro_attribute]
+pub fn hx_boosted_by_async(args: TokenStream, input: TokenStream) -> TokenStream {
+    boosted_by_async(args.into(), input.into()).into()
 }
