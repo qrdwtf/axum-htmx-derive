4ca1f2c37766463e98cf8a3a06c5c538fa5f531d
Dan Alekseev <i@qrd.wtf>
2024-04-03 11:35:09 +0400

    feat: boosted_by_async

---
+++ axum-htmx-derive-core/src/boosted_by_async.rs
@@ -0,0 +1,39 @@
+use quote::quote;
+use syn::{parse_quote, parse_str, ItemFn};
+
+pub fn transform_fn(layout_fn: String, item_fn: &mut ItemFn) -> ItemFn {
+    // println!("input code  : {}", quote!(#item_fn));
+    // println!("input syntax: {:?}", item_fn);
+
+    // function template
+    let template_fn: ItemFn = parse_quote!(
+        fn index(axum_htmx::HxBoosted(boosted): axum_htmx::HxBoosted) {
+            if boosted {
+                result_boosted
+            } else {
+                layout_fn(result_with_layout).await
+            }
+        }
+    );
+
+    // add HxBoosted input to item_fn
+    let hx_boosted_input = template_fn.sig.inputs.first().unwrap().clone();
+    item_fn.sig.inputs.push(hx_boosted_input);
+
+    // pop the last statement and wrap it with if-else
+    let modify_stmt = item_fn.block.stmts.pop().unwrap();
+    let modify_stmt = quote!(#modify_stmt).to_string();
+    let new_fn_str = quote!(#template_fn)
+        .to_string()
+        .replace("layout_fn", layout_fn.as_str())
+        .replace("result_boosted", modify_stmt.as_str())
+        .replace("result_with_layout", modify_stmt.as_str());
+
+    let new_fn: ItemFn = parse_str(new_fn_str.as_str()).unwrap();
+    let new_fn_stmt = new_fn.block.stmts.first().unwrap().clone();
+
+    // push the new statement to item_fn
+    item_fn.block.stmts.push(new_fn_stmt);
+
+    item_fn.to_owned()
+}
--- axum-htmx-derive-core/src/lib.rs
+++ axum-htmx-derive-core/src/lib.rs
@@ -5,7 +5,10 @@ mod tests;
 use proc_macro2::TokenStream;
 use proc_macro_error::abort;
 use quote::quote;
-use syn::{parse2, parse_quote, parse_str, ItemFn};
+use syn::{parse2, ItemFn};
+
+mod boosted_by;
+mod boosted_by_async;
 
 pub fn boosted_by(args: TokenStream, input: TokenStream) -> TokenStream {
     // get layout_fn from args
@@ -23,44 +26,28 @@ pub fn boosted_by(args: TokenStream, input: TokenStream) -> TokenStream {
     };
 
     // transform ItemFn
-    let new_item_fn = transform_fn(layout_fn, &mut source_item_fn);
+    let new_item_fn = boosted_by::transform_fn(layout_fn, &mut source_item_fn);
 
     quote!(#new_item_fn)
 }
 
-fn transform_fn(layout_fn: String, item_fn: &mut ItemFn) -> ItemFn {
-    // println!("input code  : {}", quote!(#item_fn));
-    // println!("input syntax: {:?}", item_fn);
+pub fn boosted_by_async(args: TokenStream, input: TokenStream) -> TokenStream {
+    // get layout_fn from args
+    let layout_fn = args.clone().into_iter().next().map(|x| x.to_string());
+    let layout_fn = if let Some(layout_fn) = layout_fn {
+        layout_fn
+    } else {
+        abort!(args, "boosted_by requires layout_fn as argument.");
+    };
 
-    // function template
-    let template_fn: ItemFn = parse_quote!(
-        fn index(axum_htmx::HxBoosted(boosted): axum_htmx::HxBoosted) {
-            if boosted {
-                result_boosted
-            } else {
-                layout_fn(result_with_layout)
-            }
-        }
-    );
+    // parse input as ItemFn
+    let mut source_item_fn = match parse2::<ItemFn>(input) {
+        Ok(syntax_tree) => syntax_tree,
+        Err(error) => return error.to_compile_error(),
+    };
 
-    // add HxBoosted input to item_fn
-    let hx_boosted_input = template_fn.sig.inputs.first().unwrap().clone();
-    item_fn.sig.inputs.push(hx_boosted_input);
+    // transform ItemFn
+    let new_item_fn = boosted_by_async::transform_fn(layout_fn, &mut source_item_fn);
 
-    // pop the last statement and wrap it with if-else
-    let modify_stmt = item_fn.block.stmts.pop().unwrap();
-    let modify_stmt = quote!(#modify_stmt).to_string();
-    let new_fn_str = quote!(#template_fn)
-        .to_string()
-        .replace("layout_fn", layout_fn.as_str())
-        .replace("result_boosted", modify_stmt.as_str())
-        .replace("result_with_layout", modify_stmt.as_str());
-
-    let new_fn: ItemFn = parse_str(new_fn_str.as_str()).unwrap();
-    let new_fn_stmt = new_fn.block.stmts.first().unwrap().clone();
-
-    // push the new statement to item_fn
-    item_fn.block.stmts.push(new_fn_stmt);
-
-    item_fn.to_owned()
+    quote!(#new_item_fn)
 }
